54b223009a7a40d6222d7403aed0a2bc8363191d
Fabian Fischer <fabian.fischer@siemens.com>
2021-09-28 10:33:32 +0200

    fix: use consistent variable name

--- hetzner-debian10-zfs-setup.sh
+++ hetzner-debian10-zfs-setup.sh
@@ -43,7 +43,7 @@ c_default_zfs_arc_max_mb=250
 c_default_bpool_tweaks="-o ashift=12 -O compression=lz4"
 c_default_rpool_tweaks="-o ashift=12 -O acltype=posixacl -O compression=zstd-9 -O dnodesize=auto -O relatime=on -O xattr=sa -O normalization=formD"
 c_default_hostname=terem
-c_default_network_device=eth0
+c_default_network_interface=eth0
 c_zfs_mount_dir=/mnt
 c_log_dir=$(dirname "$(mktemp)")/zfs-hetzner-vm
 c_install_log=$c_log_dir/install.log
